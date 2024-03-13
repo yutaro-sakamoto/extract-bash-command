1862b55f73d74992bd2921d6102081017309f42a
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-03-13 20:40:28 +0900

    [Feat]: accept multiple files

--- src/main.rs
+++ src/main.rs
@@ -35,71 +35,74 @@ fn main() {
 The output format is {command} {line} {column}.
 "#;
         opt var_cmd:bool=false, desc:"Extract variables storing command names";
-        param file:String, desc:"The file to extract commands from.";
+        param file:Vec<String>, desc:"The file to extract commands from.";
     }
     .parse_or_exit();
-
-    let code = match std::fs::read_to_string(args.file) {
-        Ok(code) => code,
-        Err(e) => {
-            eprintln!("Error reading file: {}", e);
-            std::process::exit(1);
-        }
-    };
-    let code_bytes = code.as_bytes();
 
     let mut parser = Parser::new();
     let bash_language = tree_sitter_bash::language();
     parser
         .set_language(bash_language)
         .expect("Error loading Bash grammar");
-    let parsed = match parser.parse(&code, None) {
-        Some(parsed) => parsed,
-        None => {
-            eprintln!("Error parsing code");
-            std::process::exit(1);
-        }
-    };
 
-    let mut command_ranges = run_query(
-        r#"
+    for file in &args.file {
+        let code = match std::fs::read_to_string(file) {
+            Ok(code) => code,
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+                std::process::exit(1);
+            }
+        };
+        let code_bytes = code.as_bytes();
+
+        let parsed = match parser.parse(&code, None) {
+            Some(parsed) => parsed,
+            None => {
+                eprintln!("Error parsing code");
+                std::process::exit(1);
+            }
+        };
+
+        let mut command_ranges = run_query(
+            r#"
     (command
         name:
             (command_name
                 (word) @command_name))
         "#,
-        "command_name",
-        &bash_language,
-        &parsed.root_node(),
-        code_bytes,
-    );
-    if args.var_cmd {
-        command_ranges.append(&mut run_query(
-            r#"
+            "command_name",
+            &bash_language,
+            &parsed.root_node(),
+            code_bytes,
+        );
+        if args.var_cmd {
+            command_ranges.append(&mut run_query(
+                r#"
         (command
             name: (command_name
               (simple_expansion
                 (variable_name)) @variable))
                 "#,
-            "variable",
-            &bash_language,
-            &parsed.root_node(),
-            code_bytes,
-        ));
-    }
-
-    command_ranges.sort_by(|(byte_range1, _), (byte_range2, _)| {
-        if byte_range1.start != byte_range2.start {
-            byte_range1.start.cmp(&byte_range2.start)
-        } else {
-            byte_range1.end.cmp(&byte_range2.end)
+                "variable",
+                &bash_language,
+                &parsed.root_node(),
+                code_bytes,
+            ));
         }
-    });
 
-    for (byte_range, range) in command_ranges {
-        let text = &code[byte_range.start..byte_range.end];
-        let line = range.start_point.row + 1;
-        let col = range.start_point.column + 1;
-        println!("{} {} {}", text, line, col);
+        command_ranges.sort_by(|(byte_range1, _), (byte_range2, _)| {
+            if byte_range1.start != byte_range2.start {
+                byte_range1.start.cmp(&byte_range2.start)
+            } else {
+                byte_range1.end.cmp(&byte_range2.end)
+            }
+        });
+
+        for (byte_range, range) in command_ranges {
+            let text = &code[byte_range.start..byte_range.end];
+            let line = range.start_point.row + 1;
+            let col = range.start_point.column + 1;
+            println!("{} {} {}", text, line, col);
+        }
     }
 }
