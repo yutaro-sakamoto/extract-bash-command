d9820e24843478849126ab5c13bda3a94b29a663
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-03-14 20:17:33 +0900

    [Doc]: Update README.md

--- test/test01.sh
+++ test/test01.sh
@@ -3,7 +3,7 @@ echo hello
 export TEST_VAR=HELLO
 echo "$(echo $TEST_VAR)"
 if [ "hello" = "world" ]; then
-    TEST_VAR=WORLD echo $TEST_VAR # echo hello
+    TEST_VAR=WORLD echo $TEST_VAR # echo world
 else
     cat <(echo hello | sed s/hello/world/g)
 fi
